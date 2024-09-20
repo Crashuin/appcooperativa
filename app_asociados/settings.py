ebc463e4bc3d03652c7aa1a4cf2c68ef38c7dffc
Hugo A. Pantoja <hugoandres272@gmail.com>
2024-09-19 20:18:40 -0500

    feat: corrección estilos

--- app_asociados/settings.py
+++ app_asociados/settings.py
@@ -123,18 +123,15 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'staticfiles/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'media'),
-)
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / "static"]  # Aquí pones tu carpeta de archivos estáticos
+
+# URLs para archivos subidos
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
